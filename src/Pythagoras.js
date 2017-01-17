c8e4a09bd92343fee84f653214fbfb881769a9c0
xm94630 <xm94630@126.com>
2017-01-17 17:38:30 +0800

    继续

--- src/Pythagoras.js
+++ src/Pythagoras.js
@@ -11,48 +11,60 @@ Math.deg = function(radians) {
   return radians * (180 / Math.PI);
 };
 
-const Pythagoras = ({ maxlvl, w, x, y, lvl, left, right }) => {
-
+const Pythagoras = ({ maxlvl, w, h, x, y, lvl, left, right }) => {
   if (lvl > maxlvl || w < 1) {
       return null;
   }
 
-  // const nextLeft = Factor*w,
-  //       nextRight = Factor*w,
-  //       d = nextLeft + nextRight + w,
-  //       A = 45,
-  //       B = 45;
-  const currentH = .5*w,
-        nextLeft = Math.sqrt(currentH*currentH + (.5*w*.5*w)),
-        nextRight = Math.sqrt(currentH*currentH + (.5*w*.5*w)),
-        A = Math.deg(Math.atan(currentH / (.5*w))),
-        B = Math.deg(Math.atan(currentH / (.5*w)));
+  const trigH = .3*w,
+        nextRight = Math.sqrt(trigH**2 + (.4*w)**2),
+        nextLeft = Math.sqrt(trigH**2 + (.6*w)**2),
+        nextHeight = .9*h,
+        A = Math.deg(Math.atan(trigH / (.6*w))),
+        B = Math.deg(Math.atan(trigH / (.4*w)));
 
 
   let rotate = '';
 
   if (left) {
-      rotate = `rotate(${-A} 0 ${w})`;
+      rotate = `rotate(${-A} 0 ${h})`;
   }else if (right) {
-      rotate = `rotate(${B} ${w} ${w})`;
+      rotate = `rotate(${B} ${w} ${h})`;
   }
 
   return (
       <g transform={`translate(${x} ${y}) ${rotate}`}>
-          <rect width={w} height={w}
+          <rect width={w} height={h}
                 x={0} y={0}
                 style={{fill: interpolateViridis(lvl/maxlvl)}} />
 
-          <Pythagoras w={nextLeft}
-                      x={w-nextLeft} y={-nextLeft} lvl={lvl+1} maxlvl={maxlvl}
-                      right />
+           <Pythagoras w={nextLeft} h={nextHeight}
+                       x={0} y={-nextHeight} lvl={lvl+1} maxlvl={maxlvl}
+                       left />
 
-          <Pythagoras w={nextRight}
-                      x={0} y={-nextRight} lvl={lvl+1} maxlvl={maxlvl}
-                      left />
+          <Pythagoras w={nextRight} h={nextHeight}
+                      x={w-nextRight} y={-nextHeight} lvl={lvl+1} maxlvl={maxlvl}
+                      right />
       </g>
   );
 };
 
 
 export default Pythagoras;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
