2f353072dc556b8b91051882a6ecb8c05033c644
xm94630 <xm94630@126.com>
2017-01-19 14:35:54 +0800

    添加了鼠标移动到svg的事件（只是简单的log），主要注意的是，react中元素选择的操作；还有就是事件绑定的位置。至于绑定事件的方法，其实就是和jquery中的on方法是非常类似的

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
 import React,{Component} from 'react';
 import logo from './star.svg';
 import Pythagoras from './Pythagoras';
+import {select as d3select, mouse as d3mouse} from 'd3-selection';
 import './App.css';
 
 /*这个版本是彩色的，带点动画的*/
 class App extends Component {
+    svg = {
+        width:1280,
+        height:600
+    }
     state = {
         currentMax: 0,
+        baseHeight:2,
+        heightFactor:0,
+        lean:0
     };
 
     realMax = 10;
 
     componentDidMount() {
+        d3select(this.refs.svg).on("mousemove",function(){
+            console.log('鼠标move')
+        })
+        console.log(this)
         this.next();
     }
 
@@ -32,7 +44,7 @@ class App extends Component {
                     <h2>This is a Pythagoras tree</h2>
                 </div>
                 <p className="App-intro">
-                    <svg width="640" height="480">
+                    <svg width="640" height="480" ref="svg">
 
                         <Pythagoras w={80}
                                     h={20}
