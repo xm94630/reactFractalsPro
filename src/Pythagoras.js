223ff93453dc6f838536dc8cbdef7c56650f7ab6
xm94630 <xm94630@126.com>
2017-01-19 22:52:12 +0800

    这个版本中加了很多细节的注释，另外最为重要的就是，添加了记忆功能的函数，这样子遇到已经计算过的，就可以不用再调用，直接从存储中获取，大大提升了效率

--- src/Pythagoras.js
+++ src/Pythagoras.js
@@ -1,28 +1,64 @@
 
-import React from 'react';
+import React,{Component} from 'react';
 import { interpolateViridis } from 'd3-scale';
 var l = function (){return console.log.apply(console,arguments);}
-
-
-
-//const Factor = .5*Math.sqrt(2);
 
 Math.deg = function(radians) {
   return radians * (180 / Math.PI);
 };
 
-const Pythagoras = ({ maxlvl, w, h, x, y, lvl, left, right }) => {
+//具有记忆功能的实现
+//这个非常有意思。以为在递归计算的时候，计算的参数都是一样的，最后得出特定的结构
+//那么在众多的调用中，就会出现重复的计算
+//这里就是把计算过一次的结果记录下来！！下次调用的时候，遇到参数一样的话，我们就可以从存储的对象中获取！！
+const memoizedCalc = function () {
+    const memo = {};
+
+    const key = ({ w, h, heightFactor, lean }) => [w, h, heightFactor, lean].join('-');
+
+    return (args) => {
+        const memoKey = key(args);
+
+        if (memo[memoKey]) {
+            return memo[memoKey];
+        }else{
+            const { w, h, heightFactor, lean } = args;
+
+            const trigH = heightFactor*w;
+
+            const result = {
+                nextRight: Math.sqrt(trigH**2 + (w * (.5+lean))**2),
+                nextLeft: Math.sqrt(trigH**2 + (w * (.5-lean))**2),
+                nextHeight: .8*h,
+                A: Math.deg(Math.atan(trigH / ((.5-lean) * w))),
+                B: Math.deg(Math.atan(trigH / ((.5+lean) * w)))
+            };11
+
+            memo[memoKey] = result;
+            return result;
+        }
+    }
+}();
+
+const Pythagoras = ({ w, h, x, y, heightFactor, lean, left, right, lvl, maxlvl }) => {
   if (lvl > maxlvl || w < 1) {
       return null;
   }
 
-  const trigH = .3*w,
-        nextRight = Math.sqrt(trigH**2 + (.4*w)**2),
-        nextLeft = Math.sqrt(trigH**2 + (.6*w)**2),
-        nextHeight = .9*h,
-        A = Math.deg(Math.atan(trigH / (.6*w))),
-        B = Math.deg(Math.atan(trigH / (.4*w)));
+  //旧版本的实现，没有记忆功能
+  /*const trigH = heightFactor*w,
+        nextRight = Math.sqrt(trigH**2 + (w*(.5+lean))**2),
+        nextLeft = Math.sqrt(trigH**2 + (w*(.5-lean))**2),
+        nextHeight = .8*h,
+        A = Math.deg(Math.atan(trigH / (w*(.5-lean)))),
+        B = Math.deg(Math.atan(trigH / (w*(.5+lean))));*/
 
+  const { nextRight, nextLeft, nextHeight, A, B } = memoizedCalc({
+      w: w,
+      h: h,
+      heightFactor: heightFactor,
+      lean: lean
+  });
 
   let rotate = '';
 
@@ -39,12 +75,18 @@ const Pythagoras = ({ maxlvl, w, h, x, y, lvl, left, right }) => {
                 style={{fill: interpolateViridis(lvl/maxlvl)}} />
 
            <Pythagoras w={nextLeft} h={nextHeight}
-                       x={0} y={-nextHeight} lvl={lvl+1} maxlvl={maxlvl}
-                       left />
+                         x={0} y={-nextHeight}
+                         lvl={lvl+1} maxlvl={maxlvl}
+                         heightFactor={heightFactor}
+                         lean={lean}
+                         left />
 
-          <Pythagoras w={nextRight} h={nextHeight}
-                      x={w-nextRight} y={-nextHeight} lvl={lvl+1} maxlvl={maxlvl}
-                      right />
+            <Pythagoras w={nextRight} h={nextHeight}
+                        x={w-nextRight} y={-nextHeight}
+                        lvl={lvl+1} maxlvl={maxlvl}
+                        heightFactor={heightFactor}
+                        lean={lean}
+                        right />
       </g>
   );
 };
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import logo from './star.svg';
 import Pythagoras from './Pythagoras';
 import {select as d3select, mouse as d3mouse} from 'd3-selection';
+import {scaleLinear} from 'd3-scale';
 import './App.css';
 
 /*这个版本是彩色的，带点动画的*/
@@ -11,19 +12,30 @@ class App extends Component {
         height:600
     }
     state = {
-        currentMax: 0,
-        baseHeight:2,
-        heightFactor:0,
-        lean:0
+        currentMax: 0,   //用作动画用的。每次增加一个层级，就增1
+        baseHeight:2,    //初始的方块的高度
+        heightFactor:0,  //这个其实就是高度的比例因子，他是相对于宽度而言。比如宽为100，heightFactor为0.5，那么那么三角形的高度就是100。
+                         //这个也是睡着鼠标的y坐标而改变的。
+                         //不过我奇怪的是，为0的时候，高度应该是没有的，在初始显示的时候，还是有一点点的高度的，这是为什么呢？
+        lean:0           //我称之为“偏量”，节点的正方形上边的一个点，在边长(假设为1)范围中移动的范围是[0-1]，那么中心点就是0.5
+                         //鼠标在左半个屏幕中移动的时候，正好对应[0-0.5]
+                         //这里用用法更加巧妙，以中心0.5作为起始点，左右偏移量就是这个lean。
     };
 
+    //注意，这个才是设置递归深度的。
+    //currentMax，这是用用在动画上的，就第一次渲染的时候，渲染第一个层级，第二次渲染第二层级...
     realMax = 10;
 
     componentDidMount() {
-        d3select(this.refs.svg).on("mousemove",function(){
-            console.log('鼠标move')
+
+        //js传统的事件绑定的方法。
+        this.refs.svg.addEventListener('click',function(){
+            console.log('点击svg');
         })
-        console.log(this)
+
+        //这个是使用 d3select 包装元素之后，使用的事件方法， d3select 的作用和 jquery 是一样的。
+        d3select(this.refs.svg).on("mousemove", this.onMouseMove.bind(this));
+
         this.next();
     }
 
@@ -31,9 +43,32 @@ class App extends Component {
         const { currentMax } = this.state;
 
         if (currentMax < this.realMax) {
-            this.setState({currentMax: currentMax + 1});
+            this.setState({currentMax: currentMax + 1}); //最后 currentMax 会达到 realMax 的值
             setTimeout(this.next.bind(this), 500);
         }
+    }
+
+    onMouseMove(event){
+        const [x,y] = d3mouse(this.refs.svg);
+
+        //这是个比例尺，后面用这个比例，来处理y的变化来改变方块的高度。
+        const scaleY = scaleLinear().domain([this.svg.height,this.svg.height/2])
+                                    .range([2,100]);
+
+        //比例尺
+        const scaleFactor = scaleLinear().domain([this.svg.height,0])
+                                         .range([0,.8]);
+
+        //比例尺
+        const scaleLean = scaleLinear().domain([0,this.svg.width/2,this.svg.width])
+                                       .range([.5,0,-.5]);
+
+        this.setState({
+            baseHeight: scaleY(y),
+            heightFactor:scaleFactor(y),
+            lean:scaleLean(x)
+        });
+
     }
 
     render() {
@@ -44,12 +79,14 @@ class App extends Component {
                     <h2>This is a Pythagoras tree</h2>
                 </div>
                 <p className="App-intro">
-                    <svg width="640" height="480" ref="svg">
+                    <svg width={this.svg.width} height={this.svg.height} ref="svg">
 
                         <Pythagoras w={80}
-                                    h={20}
-                                    x={320-50}
-                                    y={480-100}
+                                    h={this.state.baseHeight}
+                                    heightFactor={this.state.heightFactor}
+                                    lean={this.state.lean}
+                                    x={this.svg.width/2-40}
+                                    y={this.svg.height-this.state.baseHeight}
                                     lvl={0}
                                     maxlvl={this.state.currentMax}/>
 
